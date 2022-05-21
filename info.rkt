40ca8fe46df8ca914be61cc06cea1106ff86118e
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-05-21 22:54:52 +0200

    Use simpler package description

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/glossary.scrbl" ())))
-(define pkg-desc "Glossary of Racket terms and concepts")
+(define pkg-desc "Glossary of Racket concepts")
 (define version "0.1.0-dev")
 (define pkg-authors '(sschwarzer))
 (define license '(Apache-2.0 OR MIT))
