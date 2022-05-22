11ad34dfb5068d32849d9903ed9b759310fa23c8
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-05-22 17:43:56 +0200

    Fix `info.rkt`

--- info.rkt
+++ info.rkt
@@ -6,4 +6,4 @@
 (define pkg-desc "Glossary of Racket concepts")
 (define version "0.1.0-dev")
 (define pkg-authors '(sschwarzer))
-(dscribblings/glossary.scrblefine license '(Apache-2.0 OR MIT))
+(define license '(Apache-2.0 OR MIT))
