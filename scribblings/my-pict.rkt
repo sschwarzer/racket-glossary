b2892e41e39a0d89c8127b8124da00573db7c8fd
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-06-15 16:55:45 +0200

    Use internal `define`s instead of `let` forms
    I wanted to experiment with `let` instead of `define`, but internal
    defines are really easier to read. (Although I do like that `let`s make
    the scopes of bindings clearer.)

--- scribblings/my-pict.rkt
+++ scribblings/my-pict.rkt
@@ -17,21 +17,21 @@
 
 ; Return pict with text centered inside rectangle.
 (define (text-rectangle the-text)
-  (let ([text-pict (text the-text)])
-    (cc-superimpose
-      (rectangle RECTANGLE-WIDTH RECTANGLE-HEIGHT #:border-width LINE-WIDTH)
-      text-pict)))
+  (define text-pict (text the-text))
+  (cc-superimpose
+    (rectangle RECTANGLE-WIDTH RECTANGLE-HEIGHT #:border-width LINE-WIDTH)
+    text-pict))
 
 ; Return pict of a cons cell.
 (define (cons-cell car-value [cdr-value (void)])
-  (let ([car-pict (text-rectangle (~a car-value))]
-        [cdr-pict (if (void? cdr-value)
-                      (text-rectangle "")
-                      (text-rectangle (~a cdr-value)))])
-    (hc-append car-pict
-               ; Move right rectangle so that the borders of the two rectangles
-               ; overlap.
-               (translate cdr-pict (- LINE-WIDTH) 0))))
+  (define car-pict (text-rectangle (~a car-value)))
+  (define cdr-pict (if (void? cdr-value)
+                       (text-rectangle "")
+                       (text-rectangle (~a cdr-value))))
+  (hc-append car-pict
+             ; Move right rectangle so that the borders of the two rectangles
+             ; overlap.
+             (translate cdr-pict (- LINE-WIDTH) 0)))
 
 ; Return sub-picts for left and right half of cons cell pict.
 (define (cons-cell-car cons-cell)
@@ -43,13 +43,13 @@
 ; Return pict of cons cells with an arrow between cdrs and their pointed-to
 ; cars.
 (define (cons-cells . the-cons-cells)
-  (let* ([all-cons-cells (apply hc-append PICT-DISTANCE the-cons-cells)]
-         [with-arrow (lambda (current-pict cc1 cc2)
-                       (pin-arrow-line ARROW-SIZE
-                                       current-pict
-                                       (cons-cell-cdr cc1) cc-find
-                                       cc2 lc-find))])
-    (for/fold ([current-pict all-cons-cells])
-              ([cons-cell1 the-cons-cells]
-               [cons-cell2 (cdr the-cons-cells)])
-      (with-arrow current-pict cons-cell1 cons-cell2))))
+  (define all-cons-cells (apply hc-append PICT-DISTANCE the-cons-cells))
+  (define (with-arrow current-pict cons-cell1 cons-cell2)
+    (pin-arrow-line ARROW-SIZE
+                    current-pict
+                    (cons-cell-cdr cons-cell1) cc-find
+                    cons-cell2 lc-find))
+  (for/fold ([current-pict all-cons-cells])
+            ([cons-cell1 the-cons-cells]
+             [cons-cell2 (cdr the-cons-cells)])
+    (with-arrow current-pict cons-cell1 cons-cell2)))
