8fdb81ff2171e59d6d18e62c50a97c17856135f2
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-06-18 22:26:41 +0200

    Add to-be stats script
    I want to use this script to print some statistics about the glossary
    entries so far, for example the ratio of written entries in each
    category.
    
    The current file contains parsing to `entry` structs and a few helper
    functions to calculate further information from an `entry` (paragraphs,
    paragraph count, and whether the entry is only a reference to another
    entry).
    
    The file uses some heuristics. It's not completely reliable, but it
    should be enough to get an idea of the state of the glossary. In the
    end, only careful reading by a human can determine if an entry can be
    considered done.

---
+++ glossary-stats.rkt
@@ -0,0 +1,64 @@
+#lang racket/base
+
+(require
+  racket/file
+  racket/format
+  racket/function
+  racket/match
+  racket/string)
+
+(provide
+  entries-string->entries
+  entry-paragraphs
+  entry-paragraph-count
+  entry-only-reference?)
+
+(define CATEGORIES '("basic" "intermediate" "advanced"))
+
+(struct entry (title category text)
+  #:transparent)
+
+; Return `entry` from entry string.
+(define (entry-string->entry entry-string)
+  (define the-match
+    (regexp-match
+      (pregexp
+        (~a ; Everything up to last closing `}`.
+            "(?m:@glossary-entry\\{(.*)\\}\\s*$"
+            "\\s+"
+            "^  @level-(.*)\\s*$"
+            "((?s:.*)))"))
+      entry-string))
+  (match-define (list _ title category raw-text) the-match)
+  (define text
+    ; Remove trailing spaces at end of line.
+    (regexp-replace* " *\n" (string-trim raw-text) "\n"))
+  (entry title category text))
+
+; Return a list of paragraphs (strings).
+(define (entry-paragraphs entry)
+  (regexp-split "\n\n+" (entry-text entry)))
+
+; Return number of paragraphs.
+(define (entry-paragraph-count entry)
+  (length (entry-paragraphs entry)))
+
+; Return `#t` if the entry is only a reference to another entry.
+(define (entry-only-reference? entry)
+  (and (= (entry-paragraph-count entry) 1)
+       (string-prefix? (entry-text entry) "See @secref*[")))
+
+; Return list of `entry`s from a string multiple entries.
+(define (entries-string->entries entries-string)
+  (define matches
+    (regexp-match*
+      (pregexp
+        (~a "@glossary-entry\\{"
+            ".*?"
+            "(?:$|(?=@glossary-entry\\{))"))
+        entries-string))
+  (map (compose entry-string->entry string-trim) matches))
+
+; Return a list of `entry`s from a file path.
+(define (file->entries path)
+  (entries-string->entries (file->string path)))
