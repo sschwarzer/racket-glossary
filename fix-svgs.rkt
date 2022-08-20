5c500dccff5a6b21635fc7759a7a34369d892488
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-08-20 19:36:41 +0200

    Start change to `xml` library
    I ran into two issues with sxml that changed my mind on using it:
    
    - Parsing adds a `prfx1` in front of all the element names in the
      default namespace. I don't want the prefix in the output of the
      changed file and haven't found a way to remove the prefix without
      falling back to string operations on the XML string.
    
    - I couldn't get `ssax:modify` with 'insert-preceding` to work. The
      style element wasn't inserted and I don't know why.
    
    With the switch to the `xml` library, on the other hand, I'll need to
    implement the XML document modification myself, but so be it.

--- fix-svgs.rkt
+++ fix-svgs.rkt
@@ -3,10 +3,8 @@
 (require
   racket/file
   racket/format
-  sxml
-  ; For `current-sxml-warning-handler`
-  sxml/ssax/errors-and-warnings
-  sxml/sxpath)
+  xml
+  xml/path)
 
 ; Return `#t` if the XML `document` needs fixing. Otherwise return `#f`. See
 ; `fix-svg` on what "fixing" means.
@@ -15,10 +13,11 @@
 ; assume we both need to add the style information and change the `stroke`
 ; attributes.
 (define (needs-fixing? document)
+  (define document-xexpr
+    (xml->xexpr (document-element document)))
   (define style-element
-    ((sxpath "/svg/defs/style") document))
-  (equal? style-element '())
-)
+    (se-path* '(svg defs style) document-xexpr))
+  (not style-element))
 
 ; For `prefers-color-scheme` to be applied, either the system color scheme or
 ; the browser's color scheme must be set to "dark."
@@ -50,20 +49,11 @@
 ; - Change attribute values `stroke="rgb(0,0,0)"` to `stroke="currentColor"`.
 (define (fix-svg path)
   (define document
-    (call-with-input-file*
-      path
-      (lambda (port)
-        ; Hack to suppress warning
-        ;   ssax:warn: warning at position 137:
-        ;   DOCTYPE DECL svg http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd
-        ;   found and skipped
-        ; There doesn't seem to be a public API for this.
-        (parameterize ([current-sxml-warning-handler (lambda args void)])
-          (ssax:xml->sxml port '())))))
+    (with-input-from-file path read-xml))
   (cond
     [(needs-fixing? document)
      (define fixed-document (fix-document document))
-     (srl:sxml->xml fixed-document (current-output-port))]
+     (write-xml fixed-document (current-output-port))]
     [else
      (printf "path ~a is already fixed" path)]))
 
