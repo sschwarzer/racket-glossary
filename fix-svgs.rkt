d4211b52980d958daab0b44cdef4c95c69ffdb20
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2022-08-21 19:01:22 +0200

    Fix SVG image for dark mode
    The new code contains the updating for the style and color so the file
    works for light and dark mode.
    
    Contrary to what I had thought, it's necessary to change the `fill`
    attribute in addition to the `stroke` attribute (and for maybe even more
    attributes). Otherwise the text color remains black even in dark mode.
    
    TODO:
    
    - Write short-form tags (`<rect .../>` instead of `<rect ...></rect>`).
      There's a parameter for that.
    
    - Iterate over all SVG files in the `scribblings` directory and fix only
      the files that need fixing.

--- fix-svgs.rkt
+++ fix-svgs.rkt
@@ -3,6 +3,7 @@
 (require
   racket/file
   racket/format
+  racket/match
   xml
   xml/path)
 
@@ -22,24 +23,89 @@
 ; For `prefers-color-scheme` to be applied, either the system color scheme or
 ; the browser's color scheme must be set to "dark."
 ; See https://todo.sr.ht/~sschwarzer/racket-glossary/4 .
-(define COLOR_STYLES "
-  <style>
-    svg {
-      color: black;
-      background-color: white;
-    }
-    @media (prefers-color-scheme: dark) {
+(define COLOR-STYLE "
+  <defs>
+    <style>
       svg {
-        color: white;
-        background-color: black;
+        color: black;
+        background-color: white;
       }
-    }
-  </style>
- ")
+      @media (prefers-color-scheme: dark) {
+        svg {
+          color: white;
+          background-color: black;
+        }
+      }
+    </style>
+  </defs>
+")
 
-(define (fix-document document)
-  #f
-)
+; Return an updated XML object: If `xml` is the `svg` element, insert
+; the `defs`/`style` element as the first new child.
+;
+; `xml` is an XML `document`, `element` etc.
+; `path` is a list of symbols, e.g. '(svg defs font).
+(define (style-updater xml path)
+  (cond
+    [(and (element? xml)
+          (equal? path '(svg)))
+     (define style-element (xexpr->xml (string->xexpr COLOR-STYLE)))
+     (struct-copy
+       element
+       xml
+       [content (cons style-element (element-content xml))])]
+    [else
+     xml]))
+
+(define (black-updater xml path)
+  (cond
+    [(element? xml)
+     (define new-attributes
+       (for/list ([an-attribute (element-attributes xml)])
+         (match an-attribute
+           [(attribute start stop name "rgb(0,0,0)")
+            (attribute start stop name "currentColor")]
+           [_
+            an-attribute])))
+     (struct-copy
+       element
+       xml
+       [attributes new-attributes])]
+    [else
+     xml]))
+
+; Return an updated XML object: In any element, replace an attribute value pair
+; '(stroke "rgb(0,0,0)") with '(stroke "currentColor").
+;
+; `xml` is an XML `document`, `element` etc.
+; `path` is a list of symbols, e.g. '(svg defs font).
+(define (update xml updater)
+  (let visit ([xml xml]
+              [path '()])
+    (cond
+      [(document? xml)
+       (struct-copy
+         document
+         xml
+         [element (visit (document-element xml) path)])]
+      [(element? xml)
+       (define new-path (append path (list (element-name xml))))
+       (define updated-xml (updater xml new-path))
+       (define new-content
+         (for/list ([content-item (element-content updated-xml)])
+           (visit content-item new-path)))
+       (struct-copy
+         element
+         updated-xml
+         [content new-content])]
+      [else
+       xml])))
+
+; Return fixed SVG XML for dark mode support.
+(define (fix-document xml)
+  (define xml1 (update xml style-updater))
+  (define xml2 (update xml1 black-updater))
+  xml2)
 
 ; Read the SVG file at `path`. If it's not "fixed" yet, fix it and write the
 ; new version. Otherwise do nothing.
