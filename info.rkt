bac44ffebb205bcd63c2ab2e4c7a541b55e03bbf
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2023-04-23 13:27:51 +0200

    Update dependencies
    - Add missing dependencies according to Racket package server output,
      https://pkg-build.racket-lang.org/server/built/deps/racket-glossary.txt
    
    - Remove dependency `al2-test-runner`. Either this was never used and
      just copied from another project or there were tests for
      previously-exising functionality.

--- info.rkt
+++ info.rkt
@@ -1,10 +1,14 @@
 #lang info
 (define collection "racket-glossary")
-(define deps '("base"))
+(define deps '("base"
+               "data-doc"
+               "data-lib"
+               "gui-doc"
+               "gui-lib"
+               "rackunit-doc"))
 (define build-deps '("racket-doc"
                      "rackunit-lib"
-                     "scribble-lib"
-                     "al2-test-runner"))
+                     "scribble-lib"))
 (define scribblings '(("scribblings/racket-glossary.scrbl" ())))
 (define pkg-desc "Glossary of Racket concepts")
 (define version "0.1.0-dev")
